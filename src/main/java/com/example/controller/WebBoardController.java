5462369d4f6d8205c7a27b9f6ed8cb0f6d13b054
siyeon <poohsy3576@naver.com>
2017-10-18 12:50:33 +0900

    xxx

--- src/main/java/com/example/controller/WebBoardController.java
+++ src/main/java/com/example/controller/WebBoardController.java
@@ -31,19 +31,103 @@ public class WebBoardController {
 	
 	@Inject
 	WebBoardRepository repo;
+	
+	@GetMapping("/register")
+	public String registerGET(@ModelAttribute("vo")WebBoard vo ){
+		log.info("register get");
+		vo.setTitle("샘플 게시물 제목입니다....");
+		vo.setContent("내용을 처리해 봅니다 " );
+		vo.setWriter("user00");
+		
+		return "jsp/board/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerPOST(@ModelAttribute("vo")WebBoard vo, RedirectAttributes rttr){
+		
+		log.info("register post");
+		log.info("" + vo);
 
+		repo.save(vo);
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/view")
+	public String view(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+		
+		log.info("BNO: "+ bno);
+		
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+		
+		return "jsp/board/view";
+	}
+	
+	@GetMapping("/modify")
+	public String modify(Long bno, @ModelAttribute("pageVO") PageVO vo, Model model){
+		
+		log.info("MODIFY BNO: "+ bno);
+		
+		repo.findById(bno).ifPresent(board -> model.addAttribute("vo", board));
+		
+		return "jsp/board/modify";
+	}
+	
+	@PostMapping("/modify")
+	public String modifyPost(WebBoard board, PageVO vo, RedirectAttributes rttr ){
+		
+		log.info("Modify WebBoard: " + board);
+		
+		repo.findById(board.getBno()).ifPresent( origin ->{
+		 
+			origin.setTitle(board.getTitle());
+			origin.setContent(board.getContent());
+			
+			repo.save(origin);
+			rttr.addFlashAttribute("msg", "success");
+			rttr.addAttribute("bno", origin.getBno());
+		});
+		
+		rttr.addAttribute("page", vo.getPage());
+		rttr.addAttribute("size", vo.getSize());
+		rttr.addAttribute("type", vo.getType());
+		rttr.addAttribute("keyword", vo.getKeyword());
+
+		return "redirect:/board/view";
+	}
+	
+	
+	@PostMapping("/delete")
+	public String delete(Long bno, PageVO vo, RedirectAttributes rttr ){
+		
+		log.info("DELETE BNO: " + bno);
+		
+		repo.deleteById(bno);
+		
+		rttr.addFlashAttribute("msg", "success");
+
+		//페이징과 검색했던 결과로 이동하는 경우 
+		rttr.addAttribute("page", vo.getPage());
+		rttr.addAttribute("size", vo.getSize());
+		rttr.addAttribute("type", vo.getType());
+		rttr.addAttribute("keyword", vo.getKeyword());
+
+		return "redirect:/board/list";
+	}
+	
 	@GetMapping("/list")
 	public String list(@ModelAttribute("xxx")PageVO vo, Model model){
 		
 		Pageable pageable = vo.makePageable(0, "bno");
 		Predicate predicate = repo.makePredicate(vo.getType(), vo.getKeyword());
 		
-		Page<WebBoard> result = repo.findAll(predicate, pageable);
+		Page<WebBoard> pageMaker = repo.findAll(predicate, pageable);
 		
 		log.info("board/list()...");
 		log.info("PageVO=" + vo);
 		
-		model.addAttribute("pageMaker", new PageMaker(result));
+		model.addAttribute("pageMaker", new PageMaker<WebBoard>(pageMaker));
 		
 		return "jsp/board/list";
 	}
